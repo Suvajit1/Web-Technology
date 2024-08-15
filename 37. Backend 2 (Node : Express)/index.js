cce165166081e2ad86258ad13797410fada0908a
Suvajit Sadhukhan <suvajitss1107@gmail.com>
2024-08-16 03:44:46 +0530

    new update

---
+++ 37. Backend 2 (Node : Express)/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+// console.dir(app);
+
+let port = 8080;
+app.listen(port, ()=>{
+    console.log(`app is listenning on port : ${port}`);
+});
+
+// app.use((req,res)=>{
+//     // console.log(req);
+//     console.log("request received");
+//     // res.send("This is a basic response");
+
+//     // res.send({
+//     //     name : "Suvajit Sadhukhan",
+//     //     age : 24
+//     // });
+
+//     let code = "<h1>Fruits</h1><ul><li>Apple</li><li>Mango</li></ul>";  
+//     res.send(code);
+// });
+
+app.get("/", (req, res)=>{
+    res.send("you contacted root path");
+});
+
+app.get("/apple", (req, res)=>{
+    res.send("you contacted apple path");
+});
+
+app.get("/orange", (req, res)=>{
+    res.send("you contacted orange path");
+});
+
+app.get("*", (req, res)=>{
+    res.send("This path doesn't exist");
+});

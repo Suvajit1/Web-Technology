549c757a6bc36127ed2fbb4534a6993d8fe27f1f
Suvajit Sadhukhan <suvajitss1107@gmail.com>
2025-01-18 01:22:59 +0530

    raact practice 3 done

--- Practice/react-states/src/Counter.jsx
+++ Practice/react-states/src/Counter.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
 
-export default function Counter() {
-  let [count, setCount] = useState(0);
+function init(){
+    console.log("init was executed!");
+    return Math.random();
+}
 
-  let incCount = () => {
-    setCount(count + 1);
-    console.log(count);
-  };
+export default function Counter() {
+  let [count, setCount] = useState(init);
+
+//   // use call-back
+//   let incCount = () => {
+//     setCount((currCount) => {
+//       return currCount + 1;
+//     });
+//     setCount((currCount) => {
+//       return currCount + 1;
+//     });
+//     setCount((currCount) => {
+//       return currCount + 1;
+//     });
+//   };
+
+    console.log("Component was re-render");
+    let incCount = ()=>{
+        setCount(count+1);
+    }
 
   return (
     <div>
--- Practice/react-states/src/LikeButton.jsx
+++ Practice/react-states/src/LikeButton.jsx
@@ -1,5 +1,27 @@
-export default function LikeButton(){
-    return(
-        <div>Like Button!</div>
-    )
+import { useState } from "react";
+
+export default function LikeButton() {
+  let [isLike, setIsLike] = useState(false);
+  let [clicks, setClicks] = useState(0);
+
+  function toggleLike() {
+    setIsLike(!isLike);
+    setClicks(clicks+1);
+  }
+
+  let likeStyle = { color: "red" };
+
+  return (
+    <div>
+        <p>Count Clicks = {clicks}</p>
+      Like Button!
+      <p onClick={toggleLike}>
+        {isLike ? (
+          <i style={likeStyle} className="fa-solid fa-heart"></i>
+        ) : (
+          <i className="fa-regular fa-heart"></i>
+        )}
+      </p>
+    </div>
+  );
 }

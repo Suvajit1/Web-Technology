9ba03e63e1f997bcc499c49820018de8f32f4ef7
Suvajit Sadhukhan <suvajitss1107@gmail.com>
2025-01-22 03:26:07 +0530

    todo list

--- 66. React (part 4)/game-play/src/App.jsx
+++ 66. React (part 4)/game-play/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
 import LudoBoard from './LudoBoard'
+import TodoList from './TodoList'
+
 function App() {
   return (
     <>
-      <LudoBoard></LudoBoard>
+      {/* <LudoBoard></LudoBoard> */}
+      <TodoList></TodoList>
     </>
   )
 }
---
+++ 66. React (part 4)/game-play/src/TodoList.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export default function TodoList() {
+  let [todos, setTodos] = useState([{ task: "sample task", id: uuidv4() }]);
+  let [newTodo, setNewTodo] = useState("");
+
+  let addNewTask = () => {
+    setTodos([...todos, {task : newTodo, id : uuidv4()}]);
+    setNewTodo("");
+  };
+
+  let updateTodoValue = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Add a task"
+        value={newTodo}
+        onChange={updateTodoValue}
+      />
+      <br />
+      <button onClick={addNewTask}>Add Task!</button>
+      <br />
+      <br />
+      <br />
+
+      <hr />
+      <h4>Task Todo</h4>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}

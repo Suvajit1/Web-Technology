36b62d09a790b85af9d2ebe9c75ce2dbf3fa41ef
Suvajit Sadhukhan <suvajitss1107@gmail.com>
2025-01-30 23:06:16 +0530

    todo list update mark as done

--- 66. React (part 4)/game-play/src/TodoList.jsx
+++ 66. React (part 4)/game-play/src/TodoList.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export default function TodoList() {
-  let [todos, setTodos] = useState([{ task: "sample task", id: uuidv4() }]);
+  let [todos, setTodos] = useState([
+    { task: "sample task", id: uuidv4(), finish: false },
+  ]);
   let [newTodo, setNewTodo] = useState("");
 
   let addNewTask = () => {
-    setTodos([...todos, { task: newTodo, id: uuidv4() }]);
+    setTodos([...todos, { task: newTodo, id: uuidv4(), finish: false }]);
     setNewTodo("");
   };
 
@@ -39,6 +41,19 @@ export default function TodoList() {
     );
   };
 
+  let markAsDone = (id) => {
+    // console.log("done!")
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id == id) {
+          return { ...todo, finish: true };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
   return (
     <div>
       <input
@@ -58,11 +73,15 @@ export default function TodoList() {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <span>{todo.task}</span>
+            <span style={todo.finish ? { textDecoration: "line-through" } : {}}>
+              {todo.task}
+            </span>
             &nbsp;&nbsp;&nbsp;
             <button onClick={() => deleteTodo(todo.id)}>Delete!</button>
             &nbsp;&nbsp;
             <button onClick={() => upperCaseOne(todo.id)}>UpperCase One</button>
+            &nbsp;&nbsp; done{" "}
+            <input onClick={() => markAsDone(todo.id)} type="checkbox" />
           </li>
         ))}
       </ul>

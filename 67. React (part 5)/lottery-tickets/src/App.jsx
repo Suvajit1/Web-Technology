818b5f2069e7e3fb73542e96b6d18763b68b45f4
Suvajit Sadhukhan <suvajitss1107@gmail.com>
2025-03-04 02:40:59 +0530

    update

--- 67. React (part 5)/lottery-tickets/src/App.jsx
+++ 67. React (part 5)/lottery-tickets/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import Lottery from './Lottery'
-import { sum } from './helper'
+import "./App.css";
+import Lottery from "./Lottery";
+import { sum } from "./helper";
 
 function App() {
   // let winCondition = (ticket)=>{
   //   return sum(ticket) === 20;
   // }
 
-  let winCondition = (ticket)=>{
-    return ticket.every((num)=> num === ticket[0]);
-  }
+  let winCondition = (ticket) => {
+    return ticket.every((num) => num === ticket[0]);
+  };
 
   return (
     <>
@@ -18,7 +18,7 @@ function App() {
       <Ticket ticket={[2,4,7,0,5]}></Ticket> */}
       <Lottery n={3} winCondition={winCondition}></Lottery>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

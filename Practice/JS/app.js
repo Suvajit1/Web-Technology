573917241619b3063a56b68d35add0255a06d7e0
Suvajit Sadhukhan <suvajitss1107@gmail.com>
2025-03-11 18:00:43 +0530

    practice js 11

---
+++ Practice/JS/app.js
@@ -0,0 +1,52 @@
+function savetoDB(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("Succces: Data saved to DB");
+    } else {
+      reject("Failure: Data not saved to DB");
+    }
+  });
+}
+
+// savetoDB("Suvajit Sadhukhan")
+//   .then(() => {
+//     console.log("promice1 was resolved");
+//     return savetoDB("Arpan Sadhukhan")
+//       .then(() => {
+//         console.log("promice2 was resolved");
+//       })
+//       .catch(() => {
+//         console.log("promice2 was rejected");
+//       });
+//   })
+//   .catch(() => {
+//     console.log("promice1 was rejected");
+//   });
+
+// savetoDB("Suvajit")
+//   .then(() => {
+//     console.log("Promise1 was resolved");
+//     return savetoDB("Arpan");
+//   })
+//   .then(() => {
+//     console.log("Promise2 was resolved");
+//   })
+//   .catch(() => {
+//     console.log("Some promise was rejected");
+//   });
+
+savetoDB("Suvajit")
+  .then((res) => {
+    console.log(res);
+    console.log("Promise1 was resolved");
+    return savetoDB("Arpan");
+  })
+  .then((res)=>{
+    console.log(res);
+    console.log("Promise2 was resolved");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Promise1 was rejected");
+  });

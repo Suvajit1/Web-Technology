dcae19e985932ee9410ab523b85bf8bff9cdbb54
Suvajit Sadhukhan <suvajitss1107@gmail.com>
2025-03-12 03:08:21 +0530

    Backend 1

---
+++ Practice/Backend/script.js
@@ -0,0 +1,26 @@
+// let n =5;
+
+// for(let i=0; i<n; i++){
+//     console.log("hello", i);
+// }
+
+// console.log(process.argv);
+
+// let args = process.argv;
+
+// for(let i=2; i<args.length; i++){
+//     console.log(args[i]);
+// }
+
+// const someValues = require("./math");
+
+// console.log(someValues.mul(2,5));
+// console.log(someValues.PI);
+
+// const info = require("./Fruits");
+
+// console.log(info);
+
+import { sum, PI } from "./math.js";
+
+console.log(sum(1,4));
